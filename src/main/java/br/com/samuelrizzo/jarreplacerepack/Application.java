a3f7e50afe1b7211b517b1e2e5486d3bf86970dc
srizzo <rizzolabs@gmail.com>
2013-04-04 23:26:05 -0300

    initial commit

---
+++ src/main/java/br/com/samuelrizzo/jarreplacerepack/Application.java
@@ -0,0 +1,58 @@
+package br.com.samuelrizzo.jarreplacerepack;
+
+import de.schlichtherle.truezip.file.TApplication;
+import de.schlichtherle.truezip.fs.FsSyncException;
+import de.schlichtherle.truezip.key.pbe.swing.HurlingWindowFeedback;
+import de.schlichtherle.truezip.key.pbe.swing.InvalidKeyFeedback;
+
+/**
+ * An abstract class which configures the TrueZIP File* module.
+ * 
+ * @author Christian Schlichtherle
+ */
+abstract class Application<E extends Exception> extends TApplication<E> {
+
+    /**
+     * Runs the setup phase.
+     * <p>
+     * This method is {@link #run run} only once at the start of the life
+     * cycle.
+     * Its task is to configure the default behavior of the TrueZIP File* API
+     * in order to answer the following questions:
+     * <ul>
+     * <li>What are the file suffixes which shall be recognized as archive
+     *     files and hence as virtual directories?
+     * <li>Shall missing archive files and directory entries get automatically
+     *     created whenever required?
+     * </ul>
+     * <p>
+     * The implementation in the class {@link Application} configures
+     * the type of the feedback when prompting the user for keys for RAES
+     * encrypted ZIP alias ZIP.RAES alias TZP files by the Swing based
+     * prompting key manager.
+     * If this JVM is running in headless mode, then this configuration is
+     * ignored and the user is prompted by the console I/O based prompting
+     * key manager.
+     */
+    @Override
+    protected void setup() {
+        String spec = InvalidKeyFeedback.class.getName();
+        String impl = HurlingWindowFeedback.class.getName();
+        System.setProperty(spec, System.getProperty(spec, impl));
+    }
+
+    /**
+     * Overridden to make the application wait until it gets interrupted.
+     * Uncomment this method if you want to test the JMX interface with the
+     * short living sample applications of this archetype.
+     */
+    /*@Override
+    protected void sync() throws FsSyncException {
+        System.out.println("Waiting until interrupt...");
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException ex) {
+        }
+        super.sync();
+    }*/
+}
---
+++ src/main/java/br/com/samuelrizzo/jarreplacerepack/JarReplaceRepack.java
@@ -0,0 +1,109 @@
+package br.com.samuelrizzo.jarreplacerepack;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.schlichtherle.truezip.file.TFile;
+
+public class JarReplaceRepack extends Application<IOException> {
+
+    public static void main(String[] args) throws IOException {
+        System.exit(new JarReplaceRepack().run(args));
+    }
+
+    @Override
+    protected int work(String[] args) throws IOException {
+
+    	File workingDir = new File("/tmp/test");
+    	
+    	
+    	TFile[] jarFiles = new TFile(workingDir).listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.matches(".*(jar|war|ear|zip)$");
+			}
+		});
+    	
+    	File[] profiles = workingDir.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		});
+    	
+    	for (TFile workingJar : jarFiles) {
+        	for (File workingProfile : profiles) {
+
+        		TFile originalJar = new TFile(workingJar);
+                TFile destinationJar = new TFile(workingProfile, workingJar.getName());
+                
+                if(destinationJar.exists()){
+                	destinationJar.rm_r();
+                }
+
+        		File[] templates = workingProfile.listFiles(new FileFilter() {
+        			@Override
+        			public boolean accept(File file) {
+        				return file.isFile();
+        			}
+        		});
+
+                originalJar.cp_rp(destinationJar);
+        		
+        		for (File template : templates) {
+        			
+        			List<TFile> destinationFiles = findDestinationFileFor(destinationJar, template);
+        			
+        			if(destinationFiles.isEmpty()){
+        				System.out.println("No ocurrences of " + template.getName() + " found at " + destinationJar.getAbsolutePath());
+        			}
+        			
+        			for (TFile destinationFile : destinationFiles) {
+        				System.out.println(destinationFile.getPath() + " replaced with " + template.getAbsolutePath());
+                        new TFile(template).cp_rp(destinationFile);
+					}
+				}
+    		}
+		}
+        
+        return 0;
+    }
+    
+    private List<TFile> findDestinationFileFor(TFile jar, File template) {
+    	List<TFile> foundDestinations = new ArrayList<TFile>();
+    	List<TFile> jarFiles = ls_r(jar);
+    	for (TFile file : jarFiles) {
+			if(file.isFile() && file.getName().matches(template.getName())){
+				foundDestinations.add(file);
+			}
+		}
+    	return foundDestinations;
+	}
+
+	private List<TFile> ls_r(TFile file) {
+    	
+    	List<TFile> files = new ArrayList<TFile>();
+    	
+        if (!file.exists())
+            throw new IllegalArgumentException(file + " (file or directory does not exist)");
+        
+        files.add(file);
+        
+        if (file.isDirectory()) {
+            TFile[] entries = file.listFiles();
+            int l = entries.length - 1;
+            if (0 <= l) {
+                int i = 0;
+                while (i < l)
+                    files.addAll(ls_r(entries[i++]));
+                files.addAll(ls_r(entries[i]));
+            }
+        }
+        
+        return files;
+    }    
+}
