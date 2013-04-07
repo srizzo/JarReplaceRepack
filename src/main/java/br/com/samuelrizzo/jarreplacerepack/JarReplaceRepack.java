4a0f8606ecd22003736c82675776dbae19f3830c
srizzo <rizzolabs@gmail.com>
2013-04-07 09:51:49 -0300

    logging operation to file

--- src/main/java/br/com/samuelrizzo/jarreplacerepack/JarReplaceRepack.java
+++ src/main/java/br/com/samuelrizzo/jarreplacerepack/JarReplaceRepack.java
@@ -2,8 +2,10 @@ package br.com.samuelrizzo.jarreplacerepack;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.security.CodeSource;
 import java.util.ArrayList;
@@ -13,78 +15,98 @@ import de.schlichtherle.truezip.file.TFile;
 
 public class JarReplaceRepack extends Application<IOException> {
 
-    public static void main(String[] args) throws IOException {
+    private PrintWriter log;
+
+	public static void main(String[] args) throws IOException {
         System.exit(new JarReplaceRepack().run(args));
     }
 
     @Override
     protected int work(String[] args) throws IOException {
+
+    	File workingDir = new File(System.getProperty("user.dir"));
     	
-    	final File jarFile;
+    	try {
+    		
+    		log = new PrintWriter(new FileWriter(new File(workingDir, "JarReplaceRepack.log")));
+    		
+        	TFile[] jarFiles = new TFile(workingDir).listFiles(new FilenameFilter() {
+    			@Override
+    			public boolean accept(File dir, String name) {
+    				return name.matches(".*(jar|war|ear|zip)$") && !name.equals(getExecutable().getName());
+    			}
+    		});
+        	
+        	File[] profiles = workingDir.listFiles(new FileFilter() {
+    			@Override
+    			public boolean accept(File file) {
+    				return file.isDirectory();
+    			}
+    		});
+        	
+        	for (TFile workingJar : jarFiles) {
+            	for (File workingProfile : profiles) {
+
+            		TFile originalJar = new TFile(workingJar);
+                    TFile destinationJar = new TFile(workingProfile, workingJar.getName());
+                    
+                    if(destinationJar.exists()){
+                    	destinationJar.rm_r();
+                    }
+
+            		File[] templates = workingProfile.listFiles(new FileFilter() {
+            			@Override
+            			public boolean accept(File file) {
+            				return file.isFile();
+            			}
+            		});
+
+            		originalJar.cp_rp(destinationJar);
+            		System.out.println("Created " + destinationJar.getAbsolutePath());
+            		log.println("Created " + destinationJar.getAbsolutePath());
+            		
+            		for (File template : templates) {
+            			
+            			List<TFile> destinationFiles = findDestinationFileFor(destinationJar, template);
+            			
+            			if(destinationFiles.isEmpty()){
+            				System.out.println("No ocurrences of " + template.getName() + " found at " + destinationJar.getAbsolutePath());
+                    		log.println("No ocurrences of " + template.getName() + " found at " + destinationJar.getAbsolutePath());
+            			}
+            			
+            			for (TFile destinationFile : destinationFiles) {
+                            new TFile(template).cp_rp(destinationFile);
+            				System.out.println("Replaced " + destinationFile.getPath() + " with contents of " + template.getAbsolutePath());
+                    		log.println("Replaced " + destinationFile.getPath() + " with contents of " + template.getAbsolutePath());
+    					}
+    				}
+        		}
+    		}
+            
+            return 0;
+            
+		} catch (Exception e) {
+			e.printStackTrace();
+			e.printStackTrace(log);
+			return 1;
+		} finally {
+			if (log != null) {
+				log.close();
+			}
+		}
+    	
+    	
+
+    }
+
+	private File getExecutable() {
 		try {
 	    	CodeSource codeSource = JarReplaceRepack.class.getProtectionDomain().getCodeSource();
-			jarFile = new File(codeSource.getLocation().toURI().getPath());
+			return new File(codeSource.getLocation().toURI().getPath());
 		} catch (URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
-		
-		
-//    	File workingDir = new File(System.getProperty("user.dir"));
-    	File workingDir = new File("/tmp/test/");
-    	
-    	
-    	TFile[] jarFiles = new TFile(workingDir).listFiles(new FilenameFilter() {
-			@Override
-			public boolean accept(File dir, String name) {
-				return name.matches(".*(jar|war|ear|zip)$") && !name.equals(jarFile.getName());
-			}
-		});
-    	
-    	File[] profiles = workingDir.listFiles(new FileFilter() {
-			@Override
-			public boolean accept(File file) {
-				return file.isDirectory();
-			}
-		});
-    	
-    	for (TFile workingJar : jarFiles) {
-        	for (File workingProfile : profiles) {
-
-        		TFile originalJar = new TFile(workingJar);
-                TFile destinationJar = new TFile(workingProfile, workingJar.getName());
-                
-                if(destinationJar.exists()){
-                	destinationJar.rm_r();
-                }
-
-        		File[] templates = workingProfile.listFiles(new FileFilter() {
-        			@Override
-        			public boolean accept(File file) {
-        				return file.isFile();
-        			}
-        		});
-
-        		System.out.println("Created " + destinationJar.getAbsolutePath());
-                originalJar.cp_rp(destinationJar);
-        		
-        		for (File template : templates) {
-        			
-        			List<TFile> destinationFiles = findDestinationFileFor(destinationJar, template);
-        			
-        			if(destinationFiles.isEmpty()){
-        				System.out.println("No ocurrences of " + template.getName() + " found at " + destinationJar.getAbsolutePath());
-        			}
-        			
-        			for (TFile destinationFile : destinationFiles) {
-                        new TFile(template).cp_rp(destinationFile);
-        				System.out.println("Replaced " + destinationFile.getPath() + " with contents of " + template.getAbsolutePath());
-					}
-				}
-    		}
-		}
-        
-        return 0;
-    }
+	}
     
     private List<TFile> findDestinationFileFor(TFile jar, File template) {
     	List<TFile> foundDestinations = new ArrayList<TFile>();
